53b85fd901f99f70037fe5e12e928efa037378df
ars2nb <158744713+ars2nb@users.noreply.github.com>
2025-04-06 18:08:54 +0300

    Create SplashActivity.kt

---
+++ app/src/main/java/com/tutozz/blespam/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.tutozz.blespam
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val gifImageView: ImageView = findViewById(R.id.gifImageView)
+        val versionTextView: TextView = findViewById(R.id.versionTextView)
+
+        // Загружаем и отображаем GIF
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.anim)
+            .into(gifImageView)
+
+        val version = "v2.1 by ars2nb"
+        versionTextView.text = version
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1400)
+    }
+}
