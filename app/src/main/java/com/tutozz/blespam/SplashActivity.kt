b311d4eca5937d108cddbaf2fe9a8ccd3e90de14
ars2nb <158744713+ars2nb@users.noreply.github.com>
2025-04-07 12:23:22 +0300

    Update SplashActivity.kt

--- app/src/main/java/com/tutozz/blespam/SplashActivity.kt
+++ app/src/main/java/com/tutozz/blespam/SplashActivity.kt
@@ -5,10 +5,26 @@ import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
 import com.bumptech.glide.Glide
 
 class SplashActivity : AppCompatActivity() {
+
+    private val permissionRequestLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true &&
+            permissions[Manifest.permission.BLUETOOTH_SCAN] == true) {
+            proceedToMainActivity()
+        } else {
+            Toast.makeText(this, getString(R.string.perscan), Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,15 +33,38 @@ class SplashActivity : AppCompatActivity() {
         val gifImageView: ImageView = findViewById(R.id.gifImageView)
         val versionTextView: TextView = findViewById(R.id.versionTextView)
 
-        // Загружаем и отображаем GIF
         Glide.with(this)
             .asGif()
             .load(R.drawable.anim)
             .into(gifImageView)
 
-        val version = "v2.3 by ars2nb"
+        val version = "v2.4 by ars2nb"
         versionTextView.text = version
 
+        if (hasPermissions()) {
+            proceedToMainActivity()
+        } else {
+            permissionRequestLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.BLUETOOTH_SCAN
+                )
+            )
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        val fineLocationPermission = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val bluetoothScanPermission = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.BLUETOOTH_SCAN
+        ) == PackageManager.PERMISSION_GRANTED
+
+        return fineLocationPermission && bluetoothScanPermission
+    }
+
+    private fun proceedToMainActivity() {
         Handler().postDelayed({
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
