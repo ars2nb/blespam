4021cbf84546cb118d35e540b002afff09a97c54
ars2nb <158744713+ars2nb@users.noreply.github.com>
2025-07-31 13:53:41 +0300

    v2.8

---
+++ app/src/main/java/com/tutozz/blespam/AppConfig.kt
@@ -0,0 +1,6 @@
+package com.tutozz.blespam
+
+const val BUG_REPORT_API = "NONE"
+const val VERSION_CHECK_API = "NONE"
+const val SOCIAL_LINK = "https://ars3nb.ru/blespam/social"
+const val ICONCLICK = "https://ars3nb.ru/blespam/secret/"
